5d2a382b60777b8bc90094afa2d98dde5e64df28
Robert A <vazeux.robert@gmail.com>
2015-12-14 21:23:14 +0100

    increase/decrease led intensity with potentiometer

---
+++ potenciometro___led/potenciometro___led.ino
@@ -0,0 +1,16 @@
+const int led = 9;
+const int potenciometro = 4;
+
+void setup(){
+  
+  pinMode(led,OUTPUT);
+
+}
+
+void loop(){
+  
+  
+  int intensidad = (analogRead(potenciometro) / 4); // entre 0 y 1023
+  analogWrite(led, intensidad); // entre  0 y 255 por eso dividimos xd
+  
+}
